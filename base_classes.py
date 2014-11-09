33b2986dd413a7c2a2faea8f4a8425156d7b4f59
zsiki <siki@agt.bme.hu>
2014-11-09 13:13:41 +0100

    new ResultLog class added

--- base_classes.py
+++ base_classes.py
@@ -9,6 +9,10 @@
 
 import re
 import math
+import os
+import tempfile
+import datetime
+import time
 
 RO = 180 * 60 * 60 / math.pi
 PISEC = 180 * 60 * 60
@@ -335,6 +339,60 @@ class Circle(object):
         d23 = bearing(p2, p3).get_angle() + math.pi / 2.0
         return intersecLL( midp12, midp23, d12, d23 )
 
+
+class ResultLog(object):
+    """
+        File based logging for Surveying Calculations
+        Events & calculation results are logged into this file
+    """
+    def __init__(self, logfile):
+        """ initialize log file if neccessary
+            :param logfile: name of the log file it will be created if neccessary, messages are appended to the end
+        """
+        try:
+            f = open(logfile, "a")
+        except(IOError):
+            logfile = os.path.join(tempfile.gettempdir(), "SurveyingCalculation.log")
+            f = open(logfile, "a")
+        f.close()
+        self.logfile = logfile
+
+    def reset(self):
+        """
+            Delete log file
+        """
+        try:
+            os.remove(self.logfile)
+        except(OSError):
+            pass
+
+    def write(self, msg = ""):
+        """
+            Write simple message to log
+            :param msg: message to write
+        """
+        try:
+            f = open(self.logfile, "a")
+        except (IOError):
+            return
+        try:
+            f.write(msg + '\n')
+        except (IOError):
+            f.close()
+            return
+        try:
+            f.close()
+        except (IOError):
+            pass
+
+    def write_log(self, msg):
+        """
+            Write log message with date & time
+            :param msg: message to write
+        """
+        d = time.strftime("%Y-%m-%d %H:%M:%S",
+            datetime.datetime.now().timetuple())
+        self.write(d + " - " + msg)
 
 def distance2d(p1, p2):
     """
