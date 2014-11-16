9e7601164ca0bd0bd20fc38aca0410192893b785
Lászlo Csemniczky jr. <cslaszlo@digikom.hu>
2014-11-16 21:59:57 +0100

    start coding simple_dialog

--- simple_dialog.py
+++ simple_dialog.py
@@ -1,5 +1,7 @@
 from PyQt4.QtGui import QDialog
 from simple_calc import Ui_SimpleCalcDialog
+from surveying_util import *
+from calculation import Calculation
 
 class SimpleDialog(QDialog):
     """
@@ -7,5 +9,40 @@ class SimpleDialog(QDialog):
     """
     def __init__(self):
         super(SimpleDialog, self).__init__()
-        ui = Ui_SimpleCalcDialog()
-        ui.setupUi(self)
+        self.ui = Ui_SimpleCalcDialog()
+        self.ui.setupUi(self)
+
+        self.ui.CalcButton.clicked.connect(self.onCalcButton)
+        self.ui.ResetButton.clicked.connect(self.onResetButton)
+        self.ui.CloseButton.clicked.connect(self.onCloseButton)
+
+    def initDialog(self):
+        self.ui.Station1Combo.clear()
+        self.ui.Station2Combo.clear()
+        stations = get_stations()
+        if stations is not None:
+            for stn in stations:
+                self.ui.Station1Combo.addItem( stn[0] )
+                self.ui.Station2Combo.addItem( stn[0] )
+        
+    def onCalcButton(self):
+        if self.ui.OrientRadio.isChecked():
+            #Calculation.orientation(None, None)
+            pass
+        elif self.ui.RadialRadio.isChecked():
+            #Calculation.polarpoint(None, None)
+            pass
+        elif self.ui.IntersectRadio.isChecked():
+            #Calculation.intersection(None, None, None, None)
+            pass
+        elif self.ui.ResectionRadio.isChecked():
+            #Calculation.resection(None, None, None, None, None, None, None)
+            pass
+        elif self.ui.FreeRadio.isChecked():
+            pass
+    
+    def onResetButton(self):
+        self.initDialog()
+        
+    def onCloseButton(self):
+        self.accept()
