33eafa9faa1f79d4e124d524bb7af0fefa4cf7f7
Lászlo Csemniczky jr. <cslaszlo@digikom.hu>
2014-11-21 15:33:00 +0100

    add 'log' static variable to Calculation

--- calculation.py
+++ calculation.py
@@ -13,12 +13,14 @@ from PyQt4.QtGui import QMessageBox
 
 class Calculation(object):
     """ container class for all calculations """
+    
+    log = ""
 
     def __init__(self):
         pass
 
-    @staticmethod
-    def orientation(st, ref_list):
+    @classmethod
+    def orientation(cls, st, ref_list):
         """
             Orientation calculation for a station
             :param st: station (Station)
@@ -57,10 +59,8 @@ class Calculation(object):
             za = za + math.pi * 2
         
         # log results of orientation?
-        log = ResultLog("log.txt")
-        log.write()
-        log.write_log(u"Orientation - %s" % st.p.id)
-        log.write("Point num  Code         Direction    Bearing   Orient ang   Distance   e\" e\"max   E(m)")
+        cls.log += u"\nOrientation - %s\n" % st.p.id
+        cls.log += "Point num  Code         Direction    Bearing   Orient ang   Distance   e\" e\"max   E(m)\n"
         for ref in ref_list:
             emax = int(24.0 / math.sqrt( ref[2] / 1000.0))
             e = ref[3].get_angle("SEC") - Angle(za).get_angle("SEC")
@@ -69,14 +69,14 @@ class Calculation(object):
             if e<-PISEC:
                 e = e + 2*PISEC
             E = e / 206264.8 * ref[2]
-            log.write("%-10s %-10s   %9s   %9s   %9s   %8.3f %4d %4d %8.3f" % \
+            cls.log += "%-10s %-10s   %9s   %9s   %9s   %8.3f %4d %4d %8.3f\n" % \
                 (ref[1].point_id, ref[1].pc, \
                 ref[4].get_angle("DMS"), ref[5].get_angle("DMS"), \
-                ref[3].get_angle("DMS"), ref[2], int(e), emax, E) )
+                ref[3].get_angle("DMS"), ref[2], int(e), emax, E)
             if math.fabs(e) > emax:
                 QMessageBox.warning(self,u"Warning",u"Direction error over limit: %s - %s" % \
                                     (st.p.id, ref[1].point_id))
-        log.write("%-47s %s" % ("Average orientation angle",ref[3].get_angle("DMS")) )
+        cls.log +="%-47s %s\n" % ("Average orientation angle",ref[3].get_angle("DMS"))
         
         return Angle(za)
 
