1dff3a566683e835cd96b01092b96e5f42f2889d
zsiki <siki@agt.bme.hu>
2014-11-23 22:57:11 +0100

    event handling added

--- transformation_dialog.py
+++ transformation_dialog.py
@@ -1,4 +1,14 @@
-from PyQt4.QtGui import QDialog
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+    GNU Gama interface classes for Land Surveying Plug-in for QGIS
+    GPL v2.0 license
+    Copyright (C) 2014-  DigiKom Kft. http://digikom.hu
+    .. moduleauthor::Zoltan Siki <siki@agt.bme.hu>
+"""
+
+from PyQt4.QtGui import QDialog, QFileDialog
+from PyQt4.QtCore import SIGNAL
 from transformation_calc import Ui_TransformationCalcDialog
 
 from surveying_util import *
@@ -11,9 +21,6 @@ class TransformationDialog(QDialog):
         super(TransformationDialog, self).__init__()
         self.ui = Ui_TransformationCalcDialog()
         self.ui.setupUi(self)
-        self.from_points = get_known()
-        self.to_points = []
-        self.from_points = []
         self.common = []
         self.used = []
         self.ui.CloseButton.clicked.connect(self.onCloseButton)
@@ -22,6 +29,8 @@ class TransformationDialog(QDialog):
         self.ui.AddButton.clicked.connect(self.onAddButton)
         self.ui.RemoveButton.clicked.connect(self.onRemoveButton)
         self.ui.CalcButton.clicked.connect(self.onCalcButton)
+        # coordinate list changed
+        self.connect(self.ui.FromLayerComboBox, SIGNAL("currentIndexChanged(const QString&)"), self.fill_common)
 
     def showEvent(self, event):
         """
@@ -44,7 +53,16 @@ class TransformationDialog(QDialog):
         self.ui.CommonList.clear()
         self.ui.UsedList.clear()
         # select orthogonal transformation
-        self.ui.OrthogonalRadio.setChecked(True)
+        self.ui.OrthogonalRadio.setChecked(False)
+        self.ui.AffineRadio.setChecked(False)
+        self.ui.ThirdRadio.setChecked(False)
+        self.ui.FourthRadio.setChecked(False)
+        self.ui.FifthRadio.setChecked(False)
+        self.ui.OrthogonalRadio.setEnabled(False)
+        self.ui.AffineRadio.setEnabled(False)
+        self.ui.ThirdRadio.setEnabled(False)
+        self.ui.FourthRadio.setEnabled(False)
+        self.ui.FifthRadio.setEnabled(False)
 
     def onCloseButton(self):
         """
@@ -62,15 +80,13 @@ class TransformationDialog(QDialog):
         """
             Select target shape file
         """
-        fname = QFileDialog.getOpenFileName(self.iface.mainWindow(),
-            self.tr('Coordinate list'),
+        fname = QFileDialog.getOpenFileName(None, self.tr('Coordinate list'),
             filter= self.tr('Coordinate list file (*.shp);;'))
         if fname:
             self.ui.ToShapeEdit.setText(fname)
-            to_shp = QgsVectorLayer(fname, "tmp_to_shape", "ogr") 
-            from_name = self.ui.FromLayerComboBox.currentText()
-            if len(from_name):
-                pass   # TODO 
+            if len(self.ui.FromLayerComboBox.currentText()):
+                # fill common list
+                self.fill_common()
         else:
             self.ui.ToShapeEdit.setText('')
             # clear comon & used list
@@ -81,16 +97,72 @@ class TransformationDialog(QDialog):
         """
             Add point to used point in transformation
         """
-        pass
+        i = self.ui.CommonList.currentRow()
+        if i < 0:
+            return
+        item = self.ui.CommonList.takeItem(i)
+        self.ui.UsedList.addItem(item)
+        self.used.append(self.common[i])
+        del self.common[i]
+        if len(self.used) > 1:
+            self.ui.OrthogonalRadio.setEnabled(True)
+        if len(self.used) > 2:
+            self.ui.AffineRadio.setEnabled(True)
+        if len(self.used) > 9:
+            self.ui.ThirdRadio.setEnabled(True)
+        if len(self.used) > 14:
+            self.ui.FourthRadio.setEnabled(True)
+        if len(self.used) > 20:
+            self.ui.FifthRadio.setEnabled(True)
     
     def onRemoveButton(self):
         """
             Remove point from used points
         """
-        pass
+        i = self.ui.UsedList.currentRow()
+        if i < 0:
+            return
+        item = self.ui.UsedList.takeItem(i)
+        self.ui.CommonList.addItem(item)
+        self.common.append(self.used[i])
+        del self.used[i]
+        if len(self.used) < 2:
+            self.ui.OrthogonalRadio.setEnabled(False)
+        if len(self.used) < 3:
+            self.ui.AffineRadio.setEnabled(False)
+        if len(self.used) < 10:
+            self.ui.ThirdRadio.setEnabled(False)
+        if len(self.used) < 15:
+            self.ui.FourthRadio.setEnabled(False)
+        if len(self.used) < 21:
+            self.ui.FifthRadio.setEnabled(False)
 
     def onCalcButton(self):
         """
             Start transformation calculation
         """
         pass
+
+    def fill_common(self):
+        """
+            Find common points in coordinate lists
+        """
+        self.ui.UsedList.clear()
+        self.ui.CommonList.clear()
+        from_name = self.ui.FromLayerComboBox.currentText()
+        to_name = self.ui.ToShapeEdit.text()
+        if len(from_name) == 0 or len(to_name) == 0:
+            return
+        to_shp = QgsVectorLayer(to_name, "tmp_to_shape", "ogr") 
+        QgsMapLayerRegistry.instance().addMapLayer(to_shp, False)
+        to_points = get_known(2, "tmp_to_shape")
+        QgsMapLayerRegistry.instance().removeMapLayer("tmp_to_shape")
+        from_points = get_known(2, from_name)
+        self.common = []
+        self.used = []
+        for from_p in from_points:
+            if from_p in to_points:
+                self.common.append(from_p)
+        print self.common
+        for p in self.common:
+            self.ui.CommonList.addItem(p)
