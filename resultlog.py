aff93788e75e4334edda55789b660ff7e47a2383
zsiki <siki@agt.bme.hu>
2014-12-08 18:19:37 +0100

    add retry count to handle i/o errors caused by concurent write

--- resultlog.py
+++ resultlog.py
@@ -16,42 +16,51 @@ class ResultLog(object):
     """ File based logging for Surveying Calculations. Events & calculation results are logged into this file.
     """
     resultlog_message = ""
+    repeat_count = 3        # retry count for i/o operations
     
     def __init__(self, logfile):
-        """ initialize log file if neccessary
+        """ initialize log file if the given file cannot be opened for output then a SurveyingCalculation.log file in the temperary directory will be used
 
-            :param logfile: name of the log file it will be created if neccessary, messages are appended to the end
+            :param logfile: name of the log file it will be created if neccessary, messages will be appended to the end
         """
-        try:
-            f = open(logfile, "a")
-        except(IOError):
-            logfile = os.path.join(tempfile.gettempdir(), "SurveyingCalculation.log")
-            f = open(logfile, "a")
+        for i in range(repeat_count * 2):
+            try:
+                f = open(logfile, "a")
+                break
+            except(IOError):
+                f = None
+                if i == repeat_count:
+                logfile = os.path.join(tempfile.gettempdir(), "SurveyingCalculation.log")
         f.close()
         self.logfile = logfile
 
     def reset(self):
         """ Delete content of log file
         """
-        try:
-            os.remove(self.logfile)
-        except(OSError):
-            pass
+        for i in range(repeat_count):
+            try:
+                os.remove(self.logfile)
+                break
+            except(OSError):
+                pass
 
     def write(self, msg = ""):
         """ Write a  simple message to log
 
             :param msg: message to write
         """
-        try:
-            f = open(self.logfile, "a")
-        except (IOError):
-            return
-        try:
-            f.write(msg + '\n')
-        except (IOError):
-            f.close()
-            return
+        for i in range(repeat_count):
+            try:
+                f = open(self.logfile, "a")
+                for i in range(repeat_count):
+                    try:
+                        f.write(msg + '\n')
+                        break
+                    except (IOError):
+                        pass
+                break
+            except (IOError):
+                pass
         try:
             f.close()
         except (IOError):
@@ -65,4 +74,3 @@ class ResultLog(object):
         d = time.strftime("%Y-%m-%d %H:%M:%S",
             datetime.datetime.now().timetuple())
         self.write(d + " - " + msg)
-        
