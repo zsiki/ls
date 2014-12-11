9d17cab610ffdc2930a44e1061448e362882cec3
Lászlo Csemniczky jr. <cslaszlo@digikom.hu>
2014-12-11 15:56:38 +0100

    add batch plotting dialog handlers

---
+++ batch_plotting_dialog.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+.. module:: batch_plotting_dialog
+    :platform: Linux, Windows
+    :synopsis: GUI for batch plotting
+
+.. moduleauthor: Zoltan Siki <siki@agt.bme.hu>
+"""
+from PyQt4.QtGui import QDialog
+#from PyQt4.QtCore import Qt
+
+from batch_plotting import Ui_BatchPlottingDialog
+
+class BatchPlottingDialog(QDialog):
+    """ Class for batch plotting dialog
+    """
+    def __init__(self):
+        """ Initialize dialog data and event handlers
+        """
+        super(BatchPlottingDialog, self).__init__()
+        self.ui = Ui_BatchPlottingDialog()
+        self.ui.setupUi(self)
+
+        # event handlers
+        self.ui.PrintButton.clicked.connect(self.onPrintButton)
+        self.ui.TempDirButton.clicked.connect(self.onTempDirButton)
+        self.ui.CloseButton.clicked.connect(self.onCloseButton)
+        self.ui.TemplateList.setSortingEnabled(True)
+
+    def showEvent(self, event):
+        """ Reset dialog when receives a show event.
+
+            :param event: NOT USED
+        """
+        pass
+
+    def onTempDirButton(self):
+        """ Change the directory that contains print composer templates.
+        """
+        pass
+
+    def onPrintButton(self):
+        """ Batch plots selected geometry items using the selected template and scale.
+        """
+        pass
+
+    def onCloseButton(self):
+        """ Close the dialog when the Close button pushed.
+        """
+        self.accept()
+    
