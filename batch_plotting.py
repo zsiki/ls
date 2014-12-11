2b1504e5bf6451f37dc872cdf93d4e801a64684f
Lászlo Csemniczky jr. <cslaszlo@digikom.hu>
2014-12-12 00:07:54 +0100

    change print to plot

--- batch_plotting.py
+++ batch_plotting.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'batch_plotting.ui'
 #
-# Created: Thu Dec 11 15:47:27 2014
+# Created: Fri Dec 12 00:04:02 2014
 #      by: PyQt4 UI code generator 4.10.2
 #
 # WARNING! All changes made in this file will be lost!
@@ -33,14 +33,14 @@ class Ui_BatchPlottingDialog(object):
         self.TemplateLabel = QtGui.QLabel(BatchPlottingDialog)
         self.TemplateLabel.setGeometry(QtCore.QRect(10, 10, 111, 16))
         self.TemplateLabel.setObjectName(_fromUtf8("TemplateLabel"))
-        self.PrintButton = QtGui.QPushButton(BatchPlottingDialog)
-        self.PrintButton.setGeometry(QtCore.QRect(50, 260, 81, 23))
+        self.PlotButton = QtGui.QPushButton(BatchPlottingDialog)
+        self.PlotButton.setGeometry(QtCore.QRect(50, 260, 81, 23))
         sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Fixed, QtGui.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.PrintButton.sizePolicy().hasHeightForWidth())
-        self.PrintButton.setSizePolicy(sizePolicy)
-        self.PrintButton.setObjectName(_fromUtf8("PrintButton"))
+        sizePolicy.setHeightForWidth(self.PlotButton.sizePolicy().hasHeightForWidth())
+        self.PlotButton.setSizePolicy(sizePolicy)
+        self.PlotButton.setObjectName(_fromUtf8("PlotButton"))
         self.CloseButton = QtGui.QPushButton(BatchPlottingDialog)
         self.CloseButton.setGeometry(QtCore.QRect(160, 260, 81, 23))
         sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Fixed, QtGui.QSizePolicy.Fixed)
@@ -54,6 +54,7 @@ class Ui_BatchPlottingDialog(object):
         self.ScaleLabel.setObjectName(_fromUtf8("ScaleLabel"))
         self.ScaleCombo = QtGui.QComboBox(BatchPlottingDialog)
         self.ScaleCombo.setGeometry(QtCore.QRect(60, 140, 81, 22))
+        self.ScaleCombo.setInputMethodHints(QtCore.Qt.ImhDigitsOnly)
         self.ScaleCombo.setEditable(True)
         self.ScaleCombo.setObjectName(_fromUtf8("ScaleCombo"))
         self.ScaleCombo.addItem(_fromUtf8(""))
@@ -84,13 +85,13 @@ class Ui_BatchPlottingDialog(object):
         self.ScaleCombo.setCurrentIndex(3)
         QtCore.QMetaObject.connectSlotsByName(BatchPlottingDialog)
         BatchPlottingDialog.setTabOrder(self.TemplateList, self.ScaleCombo)
-        BatchPlottingDialog.setTabOrder(self.ScaleCombo, self.PrintButton)
-        BatchPlottingDialog.setTabOrder(self.PrintButton, self.CloseButton)
+        BatchPlottingDialog.setTabOrder(self.ScaleCombo, self.PlotButton)
+        BatchPlottingDialog.setTabOrder(self.PlotButton, self.CloseButton)
 
     def retranslateUi(self, BatchPlottingDialog):
         BatchPlottingDialog.setWindowTitle(_translate("BatchPlottingDialog", "Dialog", None))
         self.TemplateLabel.setText(_translate("BatchPlottingDialog", "Available templates:", None))
-        self.PrintButton.setText(_translate("BatchPlottingDialog", "Print", None))
+        self.PlotButton.setText(_translate("BatchPlottingDialog", "Plot", None))
         self.CloseButton.setText(_translate("BatchPlottingDialog", "Close", None))
         self.ScaleLabel.setText(_translate("BatchPlottingDialog", "Scale:", None))
         self.ScaleCombo.setItemText(0, _translate("BatchPlottingDialog", "100", None))
