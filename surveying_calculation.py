f5f98ba5a1e148d61784091fd36eb7e0cb189c51
Lászlo Csemniczky jr. <cslaszlo@digikom.hu>
2015-02-16 15:13:11 +0100

    modify locale file name in code

--- surveying_calculation.py
+++ surveying_calculation.py
@@ -52,7 +52,7 @@ class SurveyingCalculation:
         locale_path = os.path.join(
             self.plugin_dir,
             'i18n',
-            'surveying_calculation_{}.qm'.format(locale))
+            '{}.qm'.format(locale))
 
         if os.path.exists(locale_path):
             self.translator = QTranslator()
